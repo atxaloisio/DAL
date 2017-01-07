8e004035afab5cffb646cf39cf8589c3cf222699
Aloisio Xavier <atxaloisio@gmail.com>
2017-01-07 02:04:50 -0200

    versionamento de arquivos

---
+++ Repositorio/Repositorio.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+
+namespace DAL.Repositorio
+{
+    public class Repositorio<T> : IRepositorio<T>, IDisposable where T : class
+    {
+        private dbIntegracaoEntities Context;
+
+        protected Repositorio()
+        {
+            Context = new dbIntegracaoEntities();
+        }
+
+        public IQueryable<T> GetTodos()
+        {
+            return Context.Set<T>();
+        }
+
+        public IQueryable<T> Get(Expression<Func<T, bool>> predicate)
+        {
+            return Context.Set<T>().Where(predicate);
+        }
+
+        public T Find(params object[] key)
+        {
+            return Context.Set<T>().Find(key);
+        }
+
+        public T First(Expression<Func<T, bool>> predicate)
+        {
+            return Context.Set<T>().Where(predicate).FirstOrDefault();
+        }
+
+        public void Adicionar(T entity)
+        {
+            Context.Set<T>().Add(entity);
+        }
+
+        public void Atualizar(T entity)
+        {
+            Context.Entry(entity).State = EntityState.Modified;
+        }
+
+        public void Deletar(Func<T, bool> predicate)
+        {
+            Context.Set<T>().Where(predicate).ToList().ForEach(del => Context.Set<T>().Remove(del));
+        }
+
+        public void Commit()
+        {
+            Context.SaveChanges();
+        }
+      
+        public void Dispose()
+        {
+            if (Context != null)
+            {
+                Context.Dispose();
+            }
+            else
+            {
+                GC.SuppressFinalize(this);
+            }
+        }
+
+        
+
+        
+
+        
+
+        
+    }
+}

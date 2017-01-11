7477eba994ce8144d145686f3fbba147484e4cd6
Aloisio Xavier <atxaloisio@gmail.com>
2017-01-11 01:51:55 -0200

    Salvando alterações

--- dbIntegracao.Designer.cs
+++ dbIntegracao.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'D:\Desenv\Omie\Integracao\DAL\dbIntegracao.edmx'. 
+﻿// T4 code generation is enabled for model 'D:\Desenv\atra\DAL\dbIntegracao.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.

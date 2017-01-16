61815ae0ff9cc3fb2b4c1f2ac79ba24ffa1d1baa
Aloisio Xavier <atxaloisio@gmail.com>
2017-01-16 01:13:07 -0200

    salvando alterações

--- Repositorio/IRepositorio.cs
+++ Repositorio/IRepositorio.cs
@@ -10,7 +10,11 @@ namespace DAL
     public interface IRepositorio<T> where T:class
     {
         IQueryable<T> GetTodos();
+        IQueryable<T> GetTodos(Expression<Func<T, string>> ordem, bool desc, int page, int pageSize);
         IQueryable<T> Get(Expression<Func<T, bool>> predicate);
+        IQueryable<T> Get(Expression<Func<T, bool>> predicate,Expression<Func<T, string>> ordem, bool desc, int page, int pageSize);        
+        int getTotalRegistros(Expression<Func<T, bool>> predicate);
+        int getTotalRegistros();
         T Find(params object[] key);
         T First(Expression<Func<T, bool>> predicate);
         void Adicionar(T entity);
--- Repositorio/Repositorio.cs
+++ Repositorio/Repositorio.cs
@@ -11,11 +11,12 @@ namespace DAL
 {
     public class Repositorio<T> : IRepositorio<T>, IDisposable where T : class
     {
-        private dbIntegracaoEntities Context;
+        //Para alterar para o sqlcompact mudar aqui.
+        protected MySQLEntities Context;
 
         protected Repositorio()
         {
-            Context = new dbIntegracaoEntities();
+            Context = new MySQLEntities();
         }
 
         public IQueryable<T> GetTodos()
@@ -23,9 +24,47 @@ namespace DAL
             return Context.Set<T>();
         }
 
+        public IQueryable<T> GetTodos(Expression<Func<T, string>> ordem, bool desc, int page, int pageSize)
+        {
+            //int skipRows = (page - 1) * pageSize;
+
+            if (desc)
+            {
+                return Context.Set<T>().OrderByDescending(ordem).Skip(page).Take(pageSize);
+            }
+            else
+            {
+                return Context.Set<T>().OrderBy(ordem).Skip(page).Take(pageSize);
+            }
+        }
+
         public IQueryable<T> Get(Expression<Func<T, bool>> predicate)
         {
             return Context.Set<T>().Where(predicate);
+        }
+
+        public IQueryable<T> Get(Expression<Func<T, bool>> predicate, Expression<Func<T, string>> ordem, bool desc, int page, int pageSize)
+        {
+            //int skipRows = (page - 1) * pageSize;
+
+            if (desc)
+            {
+                return Context.Set<T>().Where(predicate).OrderByDescending(ordem).Skip(page).Take(pageSize);
+            }
+            else
+            {
+                return Context.Set<T>().Where(predicate).OrderBy(ordem).Skip(page).Take(pageSize);
+            }
+        }
+
+        public virtual int getTotalRegistros(Expression<Func<T, bool>> predicate)
+        {
+            return Context.Set<T>().Where(predicate).Count();
+        }
+
+        public virtual int getTotalRegistros()
+        {
+            return Context.Set<T>().Count();
         }
 
         public T Find(params object[] key)
@@ -57,7 +96,7 @@ namespace DAL
         {
             Context.SaveChanges();
         }
-      
+
         public void Dispose()
         {
             if (Context != null)
@@ -70,12 +109,8 @@ namespace DAL
             }
         }
 
-        
 
-        
 
-        
 
-        
     }
 }

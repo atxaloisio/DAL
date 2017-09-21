2113928796549a28346a4236addd84783cb95628
Aloisio Torres Xavier <atxaloisio@gmail.com>
2017-09-21 02:06:54 -0300

    salvando alterações

---
+++ Repositorio/Item_Livro_CaixaRepositorio.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Model;
+
+namespace DAL
+{
+    public class Item_Livro_CaixaRepositorio : Repositorio<Item_Livro_Caixa>, IItem_Livro_CaixaRepositorio
+    {
+    }
+}

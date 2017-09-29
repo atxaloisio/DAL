ee338a46d2359e349c5b2a7b8c7a54bd7b5aa1ee
Aloisio Torres Xavier <atxaloisio@gmail.com>
2017-09-29 02:03:33 -0300

    Salvando alterações

---
+++ Repositorio/Contas_PagarRepositorio.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Model;
+
+namespace DAL
+{
+    public class Contas_PagarRepositorio : Repositorio<Contas_Pagar>, IContas_PagarRepositorio
+    {
+    }
+}
